918b482729b540f87d1397221ed4c9bf70c0263d
rachanonk <skrrachanon@gmail.com>
2015-11-19 18:26:02 +0700

    SearchView change to EditText and coding location selection

---
+++ app/src/main/java/tk/lenkyun/foodbook/foodbook/commentActivity.java
@@ -0,0 +1,13 @@
+package tk.lenkyun.foodbook.foodbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+class CommentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
